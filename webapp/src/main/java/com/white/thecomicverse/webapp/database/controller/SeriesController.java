3457bc6e6c0f8d4734d1df11393dd07e6a300d27
codinghim <himchan.yun@stonybrook.edu>
2019-04-11 00:10:45 -0400

    SeriesController

---
+++ webapp/src/main/java/com/white/thecomicverse/webapp/database/controller/SeriesController.java
@@ -0,0 +1,50 @@
+package com.white.thecomicverse.webapp.database.controller;
+
+import com.white.thecomicverse.webapp.database.model.Series;
+import com.white.thecomicverse.webapp.database.repositories.SeriesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller    // This means that this class is a Controller
+@RequestMapping(path="/series")
+public class SeriesController {
+    @Autowired
+    private SeriesRepository seriesRepository;
+
+    @RequestMapping(value="/createSeries") // Map ONLY GET Requests
+    public String addNewLogin (HttpServletRequest req, @RequestParam(value = "seriesName") String seriesName, @RequestParam(value = "category") String category) {
+        // @ResponseBody means the returned String is the response, not a view name
+        // @RequestParam means it is a parameter from the GET or POST request
+
+        for (Series series : seriesRepository.findAll()){
+            if (series.getSeriesName().equals(seriesName)){
+                return "redirect:/create_comic_series?seriesNameExist";
+            }
+        }
+
+        Series newSeries = new Series();
+        newSeries.setSeriesName(seriesName);
+        newSeries.setAuthor("Dummy Author");
+        newSeries.setCategories(category);
+        this.seriesRepository.save(newSeries);
+        return "redirect:/create_comic_series?success";
+
+    }
+
+    @RequestMapping(value="/checkSeriesName") // Map ONLY GET Requests
+    public String addNewLogin (HttpServletRequest req, @RequestParam(value = "seriesName") String seriesName) {
+
+        for (Series series : seriesRepository.findAll()){
+            if (series.getSeriesName().equals(seriesName)){
+                return "redirect:/create_comic_series?seriesNameExist";
+            }
+        }
+        return "redirect:/create_comic_series?seriesNameNotExist";
+
+    }
+
+}
