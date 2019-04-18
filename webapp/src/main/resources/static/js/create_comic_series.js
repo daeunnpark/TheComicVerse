80e9a2349590625885598108bd71dd6f026a5fc7
Thomas Vetere <thomas.vetere@stonybrook.edu>
2019-04-18 18:03:49 -0400

    added encoding of thumbnail image to base64, base64 sent to backend encoded password input on home page

--- webapp/src/main/resources/static/js/create_comic_series.js
+++ webapp/src/main/resources/static/js/create_comic_series.js
@@ -15,6 +15,16 @@ $(".dropdown-menu a").click(function() {
   $("#categories").val($(this).data("value"));
 });
 
+function encodeImageFileAsURL() {
+  var file = document.getElementById("fileThumbnailInput").files[0];
+  var reader = new FileReader();
+  reader.onloadend = function() {
+    console.log('RESULT', reader.result);
+    document.getElementById("fileByteData").value=reader.result;
+  };
+  reader.readAsDataURL(file);
+}
+
 /*
 
 function getUsername() {
