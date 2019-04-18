c7a885e3cae340c9a32e1d0028d229dba5c66e73
Thomas Vetere <thomas.vetere@stonybrook.edu>
2019-04-18 19:08:45 -0400

    added functionality to encode image on upload episode

---
+++ webapp/src/main/resources/static/js/upload_episode.js
@@ -0,0 +1,9 @@
+function encodeImageFileAsURL() {
+    var file = document.getElementById("fileEpisodeThumbnailInput").files[0];
+    var reader = new FileReader();
+    reader.onloadend = function() {
+        console.log('RESULT', reader.result);
+        document.getElementById("fileEpisodeByteData").value=reader.result;
+    };
+    reader.readAsDataURL(file);
+}
