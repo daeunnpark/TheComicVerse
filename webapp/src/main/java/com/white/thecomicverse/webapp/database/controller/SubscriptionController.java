914c7d07a4f2f62fb025c8bd8baba2defc259968
Seok Hoon Kim <SeokHoonKim@Seoks-MBP.home>
2019-04-18 04:36:36 -0400

    subscription done

--- webapp/src/main/java/com/white/thecomicverse/webapp/database/controller/SubscriptionController.java
+++ webapp/src/main/java/com/white/thecomicverse/webapp/database/controller/SubscriptionController.java
@@ -3,6 +3,7 @@
 package com.white.thecomicverse.webapp.database.controller;
 
 
+import com.white.thecomicverse.webapp.database.repositories.SeriesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +12,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import com.white.thecomicverse.webapp.database.model.subscription;
+import com.white.thecomicverse.webapp.database.model.Series;
+
 import com.white.thecomicverse.webapp.database.repositories.SubscriptionRepository;
 import java.util.List;
 import java.util.Iterator;
+import java.util.Date;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller    // This means that this class is a Controller
@@ -22,32 +26,30 @@ public class SubscriptionController {
 
     @Autowired
     private SubscriptionRepository subscriptionRepository;
-/*
+    private SeriesRepository seriesRepository;
 
-    @RequestMapping(value="/checkSubscription")
-    public String addNewLogin (HttpServletRequest req, @RequestParam(value = "email") String email, @RequestParam(value = "username") String username
-            ,@RequestParam(value = "password") String password) {
+    @RequestMapping(value="/addSubscription")
+    public ModelAndView subscribe (HttpServletRequest req, @RequestParam(value = "username") String username, @RequestParam(value = "SeriesName") String seriesID) {
         // @ResponseBody means the returned String is the response, not a view name
         // @RequestParam means it is a parameter from the GET or POST request
-
-        for (Login login : loginRepository.findAll()){
-            if (login.getUsername().equals(username)){
-                return "redirect:/sign_up_form?userNameExist";
-            }
-            if (login.getEmail().equals(email)){
-                return "redirect:/sign_up_form?EmailExist";
-            }
+        Date d = new Date();
+        subscription sub = new subscription();
+        sub.setDate(d.toGMTString());
+        sub.setSeriesID(seriesID);
+        sub.setUsername(username);
+        this.subscriptionRepository.save(sub);
+        Series s = new Series();
+        for (Series series : seriesRepository.findAll()) {
+            s= series;
         }
 
-        Login l = new Login();
-        l.setEmail(email);
-        l.setusername(username);
-        l.setPassword(password);
-        this.subscriptionRepository.save(l);
-        return "redirect:/home";
+        ModelAndView mv = new ModelAndView("view_comic_series");
+        mv.addObject(s);
+        return mv;
+
 
     }
-*/
+
 /*
     @RequestMapping(value="/addLogin") // Map ONLY GET Requests
     public String addNewLogin (HttpServletRequest req, @RequestParam(value = "email") String email, @RequestParam(value = "username") String username
