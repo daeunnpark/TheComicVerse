fac044297e80753bbd3b947ef7355dfac408f470
Seok Hoon Kim <SeokHoonKim@Seoks-MacBook-Pro.local>
2019-05-03 15:30:43 -0400

    backed comments

---
+++ webapp/src/main/java/com/white/thecomicverse/webapp/database/model/Comments.java
@@ -0,0 +1,43 @@
+package com.white.thecomicverse.webapp.database.model;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Blob;
+import java.sql.SQLException;
+import javax.sql.rowset.serial.SerialBlob;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class Comments {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private int CommentID;
+
+    private String author;
+
+    private String text;
+
+    public int getCommentID() {
+        return CommentID;
+    }
+
+    public void setCommentID(int commentID) {
+        CommentID = commentID;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
