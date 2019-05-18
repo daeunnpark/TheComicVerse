065333d8459dba1846152c3a02d268cb345fccd3
Thomas Vetere <thomas.vetere@stonybrook.edu>
2019-05-18 19:15:02 -0400

    merge fix

--- webapp/src/main/resources/static/js/read_episode.js
+++ webapp/src/main/resources/static/js/read_episode.js
@@ -4,8 +4,5 @@ $(document).ready(function () {
     ).value = window.sessionStorage.getItem("username");
     document.getElementById("userNameInput1").value = window.sessionStorage.getItem("username");
     document.getElementById("userNameInput2").value = window.sessionStorage.getItem("username");
-<<<<<<< HEAD
-}
-=======
 });
->>>>>>> a225236ab90f813167a30d95585f5cba9b84f350
+
--- webapp/src/main/resources/static/js/view_comic_series.js
+++ webapp/src/main/resources/static/js/view_comic_series.js
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 $(document).ready(function() {
     document.getElementById("userNameInput1").value = window.sessionStorage.getItem("username");
     document.getElementById("userNameInput2").value = window.sessionStorage.getItem("username");
-}
-=======
-$(document).ready(function () {
     document.getElementById(
         "dummyUsernameForm5"
     ).value = window.sessionStorage.getItem("username");
@@ -12,4 +8,3 @@ $(document).ready(function () {
         "dummyUsernameForm4"
     ).value = window.sessionStorage.getItem("username");
 });
->>>>>>> a225236ab90f813167a30d95585f5cba9b84f350
